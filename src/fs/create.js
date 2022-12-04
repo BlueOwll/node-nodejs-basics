82b3f0b85bf7a8ca580087983a014c629d310a04
BlueOwll <blueowl.n@gmail.com>
2022-12-04 23:31:18 +0300

    feat: implement function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+// create.js - implement function that creates new file fresh.txt with content I am fresh and young inside of the files folder
+//  (if file already exists Error with message FS operation failed must be thrown)
+
+import fsp from 'fs/promises';
+import pth from 'path';
 const create = async () => {
-    // Write your code here 
+  const path = pth.join('src', 'fs', 'files', 'fresh.txt');
+  const data = 'I am fresh and young';
+  await fsp.access(path)
+    .then(() => { throw new Error('file exists') })
+    .catch((e) => { 
+      if (e.message !== 'file exists') { 
+        fsp.appendFile(path, data); 
+      } else 
+      throw new Error('FS operation failed') })
+
 };
 
 await create();
