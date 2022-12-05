360c46f05b8c73c46901fbe604e0e291739967eb
BlueOwll <blueowl.n@gmail.com>
2022-12-05 22:24:17 +0300

    feat: implement cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+// env.js - implement function that parses environment variables with prefix RSS_ and prints them
+//  to the console in the format RSS_name1=value1; RSS_name2=value2
 const parseEnv = () => {
-    // Write your code here 
-};
-
+    process.env.RSS_name1 = 'ahoy1';
+    process.env.RSS_name2 = 'ahoy2';
+    process.env.RS_name3 = 'ahoy2';
+  const pref = 'RSS_';
+	Object.keys(process.env).filter((item)=> item.includes(pref)).map((item)=> {console.log(`${item}=${process.env[item]}`)})
+}
 parseEnv();
