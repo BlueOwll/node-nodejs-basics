59b666bc900de3a498bdb6d36b914381a06d3512
BlueOwll <blueowl.n@gmail.com>
2022-12-06 00:06:39 +0300

    feat: implement stream write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+// write.js - implement function that writes process.stdin data into file fileToWrite.txt content 
+// using Writable Stream
+
+import fs from 'fs';
+import url from 'url';
+import pth from 'path';
+
+const __dirname = pth.dirname(url.fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const dstPath = pth.join(__dirname,'files', 'fileToWrite.txt');
+    const writeStream = fs.createWriteStream(dstPath, {flags: 'a' });
+    
+    console.log('enter something');
+    process.stdin.on('data', (data) => {
+        writeStream.write(data.toString());
+    });
 };
 
 await write();
