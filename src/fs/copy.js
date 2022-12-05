91d1a31e8025eeec7537d3064b8eb76628be7b40
BlueOwll <blueowl.n@gmail.com>
2022-12-05 22:02:12 +0300

    feat: implement copy & rename

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -3,38 +3,11 @@
 import fsp from 'fs/promises';
 import pth from 'path';
 const copy = async () => {
+  const srcPath = pth.join('src', 'fs', 'files');
+const dstPath = pth.join('src', 'fs', 'files_copy');	
+ 	await fsp.access(srcPath)
+	.then(()=> fsp.cp(srcPath, dstPath, {errorOnExist: true, force: false, recursive: true}))
+	.catch(()=>{throw new Error('FS operation failed');})
 
-  const srcDirPath = 'files';
-  const destDirPath = 'files_copy';
-
-
-  let dirList;
-  try{
-    // console.log('reading dir' +srcDirPath);
-    dirList = await fsp.readdir(srcDirPath,{withFileTypes:true});
-  }catch{
-    throw new Error('FS operation failed');
-  }
-
-  //console.log('go on ' + destDirPath);
-
-  try{
-    await mkdir(destDirPath, {recursive: false});
-
-    for (const str of dirList){
-      //console.log(str.name + ' dir? ' + str.isDirectory());
-      if(str.isDirectory()){
-        await copyDir(path.join(src, str.name), path.join(dest, str.name));
-      } else{
-        const srcFilePath = path.join(srcDirPath, str.name);
-        const destFilePath = path.join(destDirPath, str.name);
-        console.log('copying file ' + str.name);
-        await copyFile(srcFilePath,destFilePath);
-      }
-    }
-  }catch (e) {
-    throw new Error('FS operation failed');
-  }
-};
-
+	}
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
-const rename = async () => {
-    // Write your code here 
-};
+// rename.js - implement function that renames file wrongFilename.txt to properFilename with extension .md 
+// (if there's no file wrongFilename.txt or properFilename.md already exists Error with message FS operation failed must be thrown)
 
+import fsp from 'fs/promises';
+import pth from 'path';
+const rename= async () => {
+  const srcPath = pth.join('src', 'fs', 'files','wrongFilename.txt');
+	const dstPath = pth.join('src', 'fs', 'files','properFilename.md');	
+    try {
+        await fsp.access(dstPath);
+        throw new Error('dst exists');
+    } catch(e) {
+        if (e.message === 'dst exists') throw new Error('FS operation failed');
+        await fsp.access(srcPath)
+	        .then(()=> fsp.rename(srcPath, dstPath))  // check the errors if exists
+	        .catch(()=>{throw new Error('FS operation failed');})
+    }
+    
+ 	
+
+	}
 await rename();
