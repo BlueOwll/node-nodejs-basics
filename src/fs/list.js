c00a6942ec556821d6ee8898500e426876e7cedc
BlueOwll <blueowl.n@gmail.com>
2022-12-05 22:06:19 +0300

    feat: implement delete & list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
-const list = async () => {
-    // Write your code here 
-};
+// list.js - implement function that prints all array of filenames from files folder into console 
+// (if files folder doesn't exists Error with message FS operation failed must be thrown)
 
+import fsp from 'fs/promises';
+import pth from 'path';
+const list = async () => {
+  const srcPath = pth.join('src', 'fs', 'files');
+	await fsp.access(srcPath)
+	.then(()=> fsp.readdir(srcPath))
+	.then((res) => {res.map((item) => {console.log(item)})})
+	.catch(()=>{throw new Error('FS operation failed');})
+
+	}
 await list();
