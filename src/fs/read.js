b184e83da7dc4cdc14768aed04ce4dbb63c04f74
BlueOwll <blueowl.n@gmail.com>
2022-12-05 23:28:56 +0300

    feat: implement calcHash

--- src/fs/read.js
+++ src/fs/read.js
@@ -5,8 +5,7 @@ import fsp from 'fs/promises';
 import pth from 'path';
 const read = async () => {
     const srcPath = pth.join('src', 'fs', 'files', 'fileToRead.txt');
-    await fsp.access(srcPath)
-        .then(() => fsp.readFile(srcPath, { encoding: 'utf8' }))
+    await fsp.readFile(srcPath, { encoding: 'utf8' })
         .then((res) => { console.log(res) })
         .catch(() => { throw new Error('FS operation failed'); })
 
